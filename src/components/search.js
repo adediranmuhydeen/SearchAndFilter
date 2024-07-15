4738f3ff26894154e8308c4148c80421a2e55b9f
Muhydeen Adebayo <adediranma@examroom.ai>
2024-07-15 21:34:27 +0100

    first commit

---
+++ src/components/search.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const FirstQUestion = () =>{
+  const fruits =[
+    "Apple",
+    "Apricot",
+    "Banana",
+    "Blueberry",
+    "Cherry",
+    "Cranberry",
+    "Date",
+    "Dragonfruit",
+    "Fig",
+    "Grape",
+    "GrapeFruit",
+    "Mango",
+  ];
+
+  return(
+    <div>
+      <input type="text" placeholder="Search her ..." />
+    </div>
+  )
+}
